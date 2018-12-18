c940e7dc2e9c53fe0925bcb6ceb86997a2117a6f
kwikadi <kwikadi@gmail.com>
2018-12-18 12:46:15 +0530

    multi file upload poc

--- serve.py
+++ serve.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+import random
+import string
+
 app = Flask(__name__)
 
+app.config['UPLOAD_FOLDER'] = './images'
+
 @app.route("/")
-def hello():
+def landing():
     return render_template("index.html")
+
+@app.route("/data", methods = ["POST"])
+def getdata():
+    random_folder = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])
+    new_folder = os.path.join(app.config['UPLOAD_FOLDER'], random_folder)
+    os.mkdir(new_folder)
+    for element in request.files.getlist("files[]"):
+        element.save(os.path.join(new_folder,secure_filename(element.filename)))
+    return redirect(url_for('landing'))
--- static/js/layout.js
+++ static/js/layout.js
@@ -1,48 +1,81 @@
-+ function($) {
-    'use strict';
+// + function($) {
+//     'use strict';
 
-    // UPLOAD CLASS DEFINITION
-    // ======================
+//     // UPLOAD CLASS DEFINITION
+//     // ======================
 
-    var dropZone = document.getElementById('drop-zone');
-    var uploadForm = document.getElementById('js-upload-form');
-    var uploadText = document.getElementById('js-upload-files');
+//     $('progress').hide();
+//     var dropZone = document.getElementById('drop-zone');
+//     var uploadForm = document.getElementById('js-upload-form');
+//     var uploadText = document.getElementById('js-upload-files');
 
-    uploadForm.onchange = function () {
-        var textVal = '';
-        for (var i=0; i<this[0]["files"].length; i++) {
-            textVal += this[0]["files"][i].name + ";";
-        }
-        uploadText.value = textVal;
-        alert(textVal);
-    };
+//     uploadForm.onchange = function () {
+//         var textVal = '';
+//         for (var i=0; i<this[0]["files"].length; i++) {
+//             textVal += this[0]["files"][i].name + ";";
+//         }
+//         uploadText.value = textVal;
+//         alert(textVal);
+//     };
 
-    var startUpload = function(files) {
-        console.log(files)
-    }
+//     var startUpload = function(files) {
+//         console.log("start");
+//         $.ajax({
+//             // Your server script to process the upload
+//             url: '/data',
+//             type: 'POST',
+    
+//             // Form data
+//             data: files,
+    
+//             // Tell jQuery not to process data or worry about content-type
+//             // You *must* include these options!
+//             cache: false,
+//             contentType: false,
+//             processData: false,
+    
+//             // Custom XMLHttpRequest
+//             xhr: function() {
+//                 var myXhr = $.ajaxSettings.xhr();
+//                 if (myXhr.upload) {
+//                     // For handling the progress of the upload
+//                     myXhr.upload.addEventListener('progress', function(e) {
+//                         if (e.lengthComputable) {
+//                             $('progress').show();
+//                             $('progress').attr({
+//                                 value: e.loaded,
+//                                 max: e.total,
+//                             });
+//                         }
+//                     } , false);
+//                 }
+//                 return myXhr;
+//             }
+//         });
+//     }
 
-    uploadForm.addEventListener('submit', function(e) {
-        var uploadFiles = document.getElementById('js-upload-files').files;
-        e.preventDefault()
-        startUpload(uploadFiles)
-    })
+//     uploadForm.addEventListener('submit', function(e) {
+//         var uploadFiles = document.getElementById('js-upload-files').files;
+//         e.preventDefault()
+//         startUpload(uploadFiles)
+//     })
 
-    dropZone.ondrop = function(e) {
-        e.preventDefault();
-        console.log("hello");
-        this.className = 'upload-drop-zone';
+//     dropZone.ondrop = function(e) {
+//         e.preventDefault();
+//         console.log("hello");
+//         this.className = 'upload-drop-zone';
 
-        startUpload(e.dataTransfer.files)
-    }
+//         startUpload(e.dataTransfer.files)
+//     }
 
-    dropZone.ondragover = function() {
-        this.className = 'upload-drop-zone drop';
-        return false;
-    }
+//     dropZone.ondragover = function() {
+//         this.className = 'upload-drop-zone drop';
+//         return false;
+//     }
 
-    dropZone.ondragleave = function() {
-        this.className = 'upload-drop-zone';
-        return false;
-    }
+//     dropZone.ondragleave = function() {
+//         this.className = 'upload-drop-zone';
+//         return false;
+//     }
 
-}(jQuery);
+// }(jQuery);
